5e824d61300e4a26abdc67bd5eda412a2aa84e0c
sajikix <rsajiki@gmail.com>
2025-09-04 12:47:47 +0900

    parserの実装

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod parser;
 use crate::{lexer::Lexer, parser::Parser};
 fn main() {
     let input = "const mf = new Intl.MessageFormat(\"en\", \"Hello {$place}!\"); mf.format({ place: \"world\" });".to_string();
+    let input2 = "const mf = new Intl.MessageFormat(\"en\", \"Hello {$place}!\");".to_string();
     let mut lexer = Lexer::new(&input);
     // 終わるまでトークンを取得し続ける
     loop {
--- src/parser.rs
+++ src/parser.rs
@@ -1,3 +1,5 @@
+use std::iter::Peekable;
+
 use crate::lexer;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -6,8 +8,16 @@ pub enum Node {
         body: Vec<Node>,
     },
     VariableDeclaration {
-        id: String,
+        id: Box<Node>,
         init: Box<Node>,
+    },
+    ExpressionStatement {
+        expression: Box<Node>,
+    },
+    AssignmentExpression {
+        operator: String,
+        left: Box<Node>,
+        right: Box<Node>,
     },
     NewExpression {
         callee: Box<Node>,
@@ -21,15 +31,25 @@ pub enum Node {
         object: Box<Node>,
         property: Box<Node>,
     },
+    ObjectExpression {
+        properties: Vec<Node>,
+    },
+    Property {
+        key: String,
+        value: Box<Node>,
+    },
     Identifier(String),
     StringLiteral(String),
     NumericLiteral(u64),
 }
 
 impl Node {
-    pub fn new_variable_declaration(id: String, init: Node) -> Self {
+    pub fn new_program(body: Vec<Node>) -> Self {
+        Node::Program { body }
+    }
+    pub fn new_variable_declaration(id: Node, init: Node) -> Self {
         Node::VariableDeclaration {
-            id,
+            id: Box::new(id),
             init: Box::new(init),
         }
     }
@@ -48,75 +68,172 @@ impl Node {
             property: Box::new(property),
         }
     }
-}
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct Program {
-    body: Vec<Node>,
-}
-
-impl Program {
-    pub fn new() -> Self {
-        Self { body: Vec::new() }
+    pub fn new_call_expression(callee: Node, arguments: Vec<Node>) -> Self {
+        Node::CallExpression {
+            callee: Box::new(callee),
+            arguments,
+        }
     }
-
-    pub fn set_body(&mut self, body: Vec<Node>) {
-        self.body = body;
+    pub fn new_expression_statement(expression: Node) -> Self {
+        Node::ExpressionStatement {
+            expression: Box::new(expression),
+        }
+    }
+    pub fn new_assignment_expression(operator: String, left: Node, right: Node) -> Self {
+        Node::AssignmentExpression {
+            operator,
+            left: Box::new(left),
+            right: Box::new(right),
+        }
+    }
+    pub fn new_identifier(name: String) -> Self {
+        Node::Identifier(name)
+    }
+    pub fn new_property(key: String, value: Node) -> Self {
+        Node::Property {
+            key,
+            value: Box::new(value),
+        }
     }
 }
 
 pub struct Parser {
-    lexer: lexer::Lexer,
-    current_token: Option<lexer::Token>,
+    lexer: Peekable<lexer::Lexer>,
 }
 
 impl Parser {
     pub fn new(src: &str) -> Self {
-        let mut lexer = lexer::Lexer::new(src);
-        let first_token = lexer.next();
-        Self {
-            lexer,
-            current_token: first_token,
+        let lexer = lexer::Lexer::new(src).peekable();
+        Self { lexer }
+    }
+
+    fn parse_literal(&mut self) -> Option<Node> {
+        let t = match self.lexer.next() {
+            Some(token) => token,
+            None => return None,
+        };
+        match t {
+            lexer::Token::StringLiteral(s) => Some(Node::new_string_literal(s)),
+            lexer::Token::Number(n) => Some(Node::NumericLiteral(n)),
+            _ => None,
         }
     }
 
-    fn consume_next_token(&mut self) {
-        self.current_token = self.lexer.next();
+    fn parse_object_expression(&mut self) -> Node {
+        let mut properties = Vec::new();
+
+        loop {
+            let t = match self.lexer.peek() {
+                Some(token) => token.clone(),
+                None => break,
+            };
+            match t {
+                lexer::Token::Identifier(name) => {
+                    self.lexer.next(); // consume identifier
+                    let key = name.clone();
+                    let t = match self.lexer.next() {
+                        Some(token) => token,
+                        None => panic!("Unexpected end of input after object key"),
+                    };
+                    match t {
+                        lexer::Token::Punctuator(':') => {
+                            let value = match self.parse_literal() {
+                                Some(v) => v,
+                                None => {
+                                    panic!("Expected primitive value after ':' in object property")
+                                }
+                            };
+                            properties.push(Node::new_property(key, value));
+                            let t = match self.lexer.peek() {
+                                Some(token) => token.clone(),
+                                None => break,
+                            };
+                            match t {
+                                lexer::Token::Punctuator(',') => {
+                                    self.lexer.next(); // consume ','
+                                }
+                                lexer::Token::Punctuator('}') => {
+                                    self.lexer.next(); // consume '}'
+                                    break;
+                                }
+                                _ => panic!("Unexpected token in object expression: {:?}", t),
+                            }
+                        }
+                        _ => panic!("Expected ':' after object key"),
+                    }
+                }
+                lexer::Token::Punctuator('}') => {
+                    self.lexer.next(); // consume '}'
+                    break;
+                }
+                _ => panic!("Unexpected token in object expression: {:?}", t),
+            }
+        }
+        Node::ObjectExpression { properties }
     }
 
     fn parse_arguments(&mut self) -> Vec<Node> {
         let mut args = Vec::new();
 
         loop {
-            match &self.current_token {
-                Some(lexer::Token::StringLiteral(s)) => {
+            let t = match self.lexer.peek() {
+                Some(token) => token.clone(),
+                None => break,
+            };
+            match t {
+                lexer::Token::StringLiteral(s) => {
                     args.push(Node::new_string_literal(s.clone()));
-                    self.consume_next_token(); // consume string literal
+                    self.lexer.next(); // consume string literal
                 }
-                Some(lexer::Token::Punctuator(',')) => {
-                    self.consume_next_token(); // consume ','
+                lexer::Token::Punctuator('{') => {
+                    self.lexer.next(); // consume '{' 
+                    args.push(self.parse_object_expression());
                 }
-                Some(lexer::Token::Punctuator(')')) => {
-                    self.consume_next_token(); // consume ')'
+                lexer::Token::Punctuator(',') => {
+                    self.lexer.next(); // consume ','
+                }
+                lexer::Token::Punctuator(')') => {
+                    self.lexer.next(); // consume ')'
                     break;
                 }
-                _ => panic!("Unexpected token in arguments: {:?}", self.current_token),
+                _ => panic!("Unexpected token in arguments: {:?}", t),
             }
         }
         args
     }
 
+    fn parse_call_expression(&mut self) -> Option<Node> {
+        let callee = self.parse_member_expression();
+        let t = match self.lexer.peek() {
+            Some(token) => token.clone(),
+            None => return None,
+        };
+        match t {
+            lexer::Token::Punctuator('(') => {
+                self.lexer.next(); // consume '('
+                let args = self.parse_arguments();
+                Some(Node::new_call_expression(callee, args))
+            }
+            _ => panic!("Expected '(' after callee in call expression"),
+        }
+    }
+
     fn parse_member_expression(&mut self) -> Node {
         let mut members = Vec::new();
+
         loop {
-            match &self.current_token {
-                Some(lexer::Token::Identifier(name)) => {
+            let t = match self.lexer.next() {
+                Some(token) => token,
+                None => panic!("Unexpected end of input while parsing member expression"),
+            };
+            match t {
+                lexer::Token::Identifier(name) => {
                     let property = name.clone();
-                    self.consume_next_token(); // consume identifier
+                    println!("Found identifier in member expression: {}", property);
                     members.push(property);
-                    match &self.current_token {
+                    match &self.lexer.peek() {
                         Some(lexer::Token::Punctuator('.')) => {
-                            self.consume_next_token(); // consume '.'
+                            self.lexer.next(); // consume '.'
                         }
                         _ => break,
                     }
@@ -133,9 +250,13 @@ impl Parser {
 
     fn parse_new_expression(&mut self) -> Node {
         let callee = self.parse_member_expression();
-        match &self.current_token {
-            Some(lexer::Token::Punctuator('(')) => {
-                self.consume_next_token(); // consume '('
+        let t = match self.lexer.peek() {
+            Some(token) => token,
+            None => panic!("Unexpected end of input after 'new' callee"),
+        };
+        match t {
+            lexer::Token::Punctuator('(') => {
+                self.lexer.next(); // consume '('
                 let args = self.parse_arguments();
                 Node::new_new_expression(callee, args)
             }
@@ -143,99 +264,114 @@ impl Parser {
         }
     }
 
-    pub fn parse_assignment_expression(&mut self) -> Node {
-        match &self.current_token {
-            Some(lexer::Token::Keyword(k)) => match k.as_str() {
+    fn parse_left_hand_side_expression(&mut self) -> Option<Node> {
+        let t = match self.lexer.peek() {
+            Some(token) => token,
+            None => return None,
+        };
+        match t {
+            lexer::Token::Keyword(k) => match k.as_str() {
                 "new" => {
                     println!("Parsing 'new' expression");
-                    self.consume_next_token(); // consume 'new'
-                    let m = self.parse_new_expression();
-                    println!("Parsed new expression: {:?}", m);
-                    m
+                    self.lexer.next(); // consume 'new'
+                    return Some(self.parse_new_expression());
                 }
                 _ => panic!("Unexpected keyword: {}", k),
             },
-            Some(lexer::Token::Identifier(name)) => {
-                let id = name.clone();
-                Node::Identifier(id)
+            lexer::Token::Identifier(_) => self.parse_call_expression(),
+            _ => self.parse_left_hand_side_expression(),
+        }
+    }
+
+    pub fn parse_assignment_expression(&mut self) -> Option<Node> {
+        let expr = self.parse_left_hand_side_expression();
+        let t = match self.lexer.peek() {
+            Some(token) => token,
+            None => return expr,
+        };
+        println!("Peeked token in assignment expression: {:?}", t);
+        match t {
+            lexer::Token::Punctuator('=') => {
+                self.lexer.next();
+                // TODO: unwrapをなんとかする
+                Some(Node::new_assignment_expression(
+                    "=".to_string(),
+                    expr.unwrap(),
+                    self.parse_assignment_expression().unwrap(),
+                ))
             }
-            Some(lexer::Token::Number(n)) => Node::NumericLiteral(*n),
-            _ => panic!("Expected identifier or number"),
+            _ => expr,
+        }
+    }
+
+    fn parse_identifier(&mut self) -> Option<Node> {
+        let t = match self.lexer.next() {
+            Some(token) => token,
+            None => return None,
+        };
+        match t {
+            lexer::Token::Identifier(name) => Some(Node::new_identifier(name)),
+            _ => None,
+        }
+    }
+
+    pub fn parse_initializer(&mut self) -> Option<Node> {
+        let t = match self.lexer.next() {
+            Some(token) => token,
+            None => return None,
+        };
+        match t {
+            lexer::Token::Punctuator(c) => match c {
+                '=' => self.parse_assignment_expression(),
+                _ => None,
+            },
+            _ => None,
         }
     }
 
     pub fn parse_variable_declaration(&mut self) -> Node {
-        // 識別子を期待
-        let id = match &self.current_token {
-            Some(lexer::Token::Identifier(name)) => name.clone(),
-            _ => panic!("Expected identifier"),
-        };
-        // 次のトークンに進む
-        self.current_token = self.lexer.next();
-
-        // '=' を期待
-        match &self.current_token {
-            Some(lexer::Token::Punctuator('=')) => {
-                // 次のトークンに進む
-                self.current_token = self.lexer.next();
-            }
-            _ => panic!("Expected '='"),
-        }
-
-        // 初期化式を解析（ここでは簡略化して識別子または数値リテラルのみを扱う）
-        let init = self.parse_assignment_expression();
-
-        // ';' を期待
-        match &self.current_token {
-            Some(lexer::Token::Punctuator(';')) => {
-                // 次のトークンに進む
-                self.current_token = self.lexer.next();
-            }
-            _ => panic!("Expected ';'"),
-        }
-
+        // TODO: unwrapをなんとかする
+        let id = self.parse_identifier().unwrap();
+        // TODO: unwrapをなんとかする
+        let init = self.parse_initializer().unwrap();
         Node::new_variable_declaration(id, init)
     }
 
-    pub fn parse_statement(&mut self) -> Node {
-        match &self.current_token {
-            Some(lexer::Token::Keyword(k)) => match k.as_str() {
+    pub fn parse_statement(&mut self) -> Option<Node> {
+        let t = match self.lexer.peek() {
+            Some(t) => t,
+            None => return None,
+        };
+        let node = match t {
+            lexer::Token::Keyword(k) => match k.as_str() {
                 "const" => {
-                    self.consume_next_token(); // consume 'const'
-                    self.parse_variable_declaration()
+                    self.lexer.next(); // consume 'const'
+                    Some(self.parse_variable_declaration())
                 }
-                _ => Node::new_string_literal("".to_string()), // ダミー
+                _ => Some(Node::new_string_literal("".to_string())), // ダミー
             },
-            _ => Node::new_string_literal("".to_string()), // ダミー
+            _ => Some(Node::new_expression_statement(
+                self.parse_assignment_expression().unwrap(),
+            )),
+        };
+        if let Some(lexer::Token::Punctuator(c)) = self.lexer.peek() {
+            // ';'を消費する
+            if c == &';' {
+                self.lexer.next();
+            }
         }
+        node
     }
 
-    pub fn parse_source_element(&mut self) -> Option<Node> {
-        if self.current_token.is_none() {
-            return None;
-        }
-
-        let node = self.parse_statement();
-
-        // 次のトークンに進む
-        self.current_token = self.lexer.next();
-
-        Some(node)
-    }
-
-    pub fn parse(&mut self) -> Program {
-        let mut program = Program::new();
-
+    pub fn parse(&mut self) -> Node {
         let mut body = Vec::new();
-
         loop {
-            let node = self.parse_source_element();
+            let node = self.parse_statement();
 
             match node {
                 Some(n) => body.push(n),
                 None => {
-                    program.set_body(body);
-                    return program;
+                    return Node::new_program(body.clone());
                 }
             }
         }
